7ffc07c3c91cf15b150016fdc9172c3d10d6e54b
Vikas Yadav <web.yvikas@gmail.com>
2022-05-31 14:14:50 +0530

    fixed some more issues

--- src/ItemDetail/ItemDetail.js
+++ src/ItemDetail/ItemDetail.js
@@ -13,7 +13,7 @@ function ItemDetail() {
     <div class="lg:w-4/5 mx-auto flex flex-wrap">
       <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400"/>
       <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
+        <h2 class="text-sm title-font text-gray-500 tracking-widest"> WORK IN PROGRESS !!!!!!!!!!!!!!!!!!!</h2>
         <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
         <div class="flex mb-4">
           <span class="flex items-center">
